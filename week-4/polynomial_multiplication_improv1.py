c7ad929dd9253b633de1056cb92e849aeeeec9fe
Harry John <hajohn@microsoft.com>
2017-03-03 08:12:44 +0530

    Completed first improv of polynomial multiplication and surprisingly got it correct

--- week-4/polynomial_multiplication_improv1.py
+++ week-4/polynomial_multiplication_improv1.py
@@ -1,90 +1,111 @@
+def match_length(poly1, poly2):
+    """Returns a padded version of a list"""
+    ret_poly1 = poly1
+    ret_poly2 = poly2
+
+    if len(poly1) < len(poly2):
+        ret_poly1 = [0] * (len(poly2) - len(poly1)) + ret_poly1
+    else:
+        ret_poly2 = [0] * (len(poly1) - len(poly2)) + ret_poly2
+
+    return (ret_poly1, ret_poly2)
+
+def remove_leading_zeros(poly):
+    """Remove the leading zeroes in a poly of an array"""
+    for i in range(len(poly)):
+        if poly[i] != 0:
+            break
+
+    # Return the non zero part of the polynomial
+    return poly[i:]
+
 class Polynomial:
-	"""Polynomial Definiton"""
+    """Polynomial Definiton"""
 
-	def __init__(self, polynomial):
-		"""Constructor"""
-		self.__polynomial = polynomial
+    def __init__(self, polynomial):
+        """Constructor"""
+        self.__polynomial = remove_leading_zeros(polynomial)
 
-	def __add__(self, other):
-		"""Polynomial Addition"""
-		(poly1, poly2) = Polynomial._match_length(self.__polynomial, other.__polynomial)
-		
-		poly_sum = []
-		for (c1, c2) in zip(poly1, poly2):
-			poly_sum.append(c1 + c2)
+    def __add__(self, other):
+        """Polynomial Addition"""
+        (poly1, poly2) = match_length(self.__polynomial, other.get_internal_notation())
 
-		return Polynomial(poly_sum)
+        poly_sum = []
+        for (c1, c2) in zip(poly1, poly2):
+            poly_sum.append(c1 + c2)
 
-	def __mul__(self, other):
-		"""Polynomial Multiplication"""
-		(poly1, poly2) = Polynomial._match_length(self.__polynomial, other.__polynomial)
+        return Polynomial(poly_sum)
 
-		if(len(poly1)) == 0:
-			return Polynomial([0])
-		
-		if len(poly1) == 1:
-			return Polynomial([poly1[0] * poly2[0]])
+    def __mul__(self, other):
+        """Polynomial Multiplication"""
+        (poly1, poly2) = match_length(self.__polynomial, other.get_internal_notation())
 
-		n = len(poly1)
-		n_by_2 = len(poly1) // 2
+        if(len(poly1)) == 0:
+            return Polynomial([0])
 
-		d0 = Polynomial(poly1[(n - n_by_2):])
-		d1 = Polynomial(poly1[:(n - n_by_2)])
+        if len(poly1) == 1:
+            return Polynomial([poly1[0] * poly2[0]])
 
-		e0 = Polynomial(poly2[(n - n_by_2):])
-		e1 = Polynomial(poly2[:(n - n_by_2)])
+        n = len(poly1)
+        n_by_2 = len(poly1) // 2
 
-		print(poly1, d0, d1)
-		print(poly2, e0, e1)
+        vd0 = Polynomial(poly1[(n - n_by_2):])
+        vd1 = Polynomial(poly1[:(n - n_by_2)])
 
-		return 0
+        ve0 = Polynomial(poly2[(n - n_by_2):])
+        ve1 = Polynomial(poly2[:(n - n_by_2)])
 
-	def __str__(self):
-		"""Returns string representaion
-		of a polynomial
-		"""
-		term_list = []
-		max_power = len(self.__polynomial) - 1
-		for coefficient in self.__polynomial:
-			if coefficient == 1 and max_power != 0:
-				term_list.append("x^%d" % (max_power,))
+        print(poly1, vd0, vd1)
+        print(poly2, ve0, ve1)
 
-			elif coefficient != 0:
-				if max_power == 0:
-					term_list.append("%d" % (coefficient,))
-				else:
-					term_list.append("%dx^%d" % (coefficient, max_power))
-			max_power -= 1
+        d1e1 = vd1 * ve1
+        d1e0 = vd1 * ve0
+        d0e1 = vd0 * ve1
+        d0e0 = vd0 * ve0
 
-		if len(term_list) == 0:
-			return "0"
+        d1e1_x_n = Polynomial(d1e1.get_internal_notation() + ([0] * n))
+        d1e0_d0e1_x_n_2 = Polynomial((d1e0 + d0e1).get_internal_notation() + ([0] * (n // 2)))
 
-		string_repr = ""
-		for i in range(len(term_list)):
-			if i > 0:
-				string_repr += " + "
-			string_repr += term_list[i]
+        return d1e1_x_n + d1e0_d0e1_x_n_2 + d0e0
 
-		return string_repr
+    def get_internal_notation(self):
+        """Returns the internal representation of a polynomial"""
+        return self.__polynomial
 
-	def _match_length(poly1, poly2):
-		"""Returns a padded version of a list"""
-		ret_poly1 = poly1
-		ret_poly2 = poly2
+    def __str__(self):
+        """Returns string representaion
+        of a polynomial
+        """
+        term_list = []
+        max_power = len(self.__polynomial) - 1
+        for coefficient in self.__polynomial:
+            if coefficient == 1 and max_power != 0:
+                term_list.append("x^%d" % (max_power,))
 
-		if len(poly1) < len(poly2):
-			ret_poly1 = [0] * (len(poly2) - len(poly1)) + ret_poly1
-		else:
-			ret_poly2 = [0] * (len(poly1) - len(poly2)) + ret_poly2
+            elif coefficient != 0:
+                if max_power == 0:
+                    term_list.append("%d" % (coefficient,))
+                else:
+                    term_list.append("%dx^%d" % (coefficient, max_power))
+            max_power -= 1
 
-		return (ret_poly1, ret_poly2)
+        if len(term_list) == 0:
+            return "0"
+
+        string_repr = ""
+        for i in range(len(term_list)):
+            if i > 0:
+                string_repr += " + "
+            string_repr += term_list[i]
+
+        return string_repr
 
 def test():
-	p1 = Polynomial([7, 6, 5, 4, 3, 2, 1])
-	p2 = Polynomial([19, 18, 17, 16, 15, 14, 13])
+    p1 = Polynomial([4, 3, 2, 1])
+    p2 = Polynomial([1, 2, 3, 4])
 
-	print(p1 * p2)
+    print(p1 * p2)
 
 if __name__ == "__main__":
-	test()
+    test()
 
